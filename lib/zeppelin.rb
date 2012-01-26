def2e8473907567682c8cbf665becb4ced0c4270
James Herdman <james.herdman@me.com>
2012-01-26 14:03:33 -0500

    Whitespace police

--- lib/zeppelin.rb
+++ lib/zeppelin.rb
@@ -3,7 +3,7 @@ require 'yajl'
 require 'time'
 
 # A very tiny Urban Airship Push Notification API client.
-# 
+#
 # Provides thin wrappers around API calls to the most common API tasks. For more
 # information on how the requests and responses are formatted, visit the [Urban
 # Airship Push Notification API docs](http://urbanairship.com/docs/push.html).
@@ -14,12 +14,12 @@ class Zeppelin
   BROADCAST_URI = '/api/push/broadcast/'
   SUCCESSFUL_STATUS_CODES = (200..299)
   JSON_HEADERS = { 'Content-Type' => 'application/json' }
-  
+
   # The connection to `https://go.urbanairship.com`.
   attr_reader :connection
-  
+
   # Creates a new client.
-  # 
+  #
   # @param [String] application_key your Urban Airship Application Key
   # @param [String] application_master_secret your Urban Airship Application
   #   Master Secret
@@ -28,111 +28,111 @@ class Zeppelin
       builder.request :json
       builder.adapter :net_http
     end
-    
+
     @connection.basic_auth(application_key, application_master_secret)
   end
-  
+
   # Registers a device token.
-  # 
+  #
   # @param [String] device_token
   # @param [Hash] payload the payload to send during registration
   # @return [Boolean] whether or not the registration was successful
   def register_device_token(device_token, payload = {})
     uri = device_token_uri(device_token)
-    
+
     if payload.empty?
       response = @connection.put(uri)
     else
       response = @connection.put(uri, payload, JSON_HEADERS)
     end
-    
+
     successful?(response)
   end
-  
+
   # Retrieves information on a device token.
-  # 
+  #
   # @param [String] device_token
   # @return [Hash, nil]
   def device_token(device_token)
     response = @connection.get(device_token_uri(device_token))
     successful?(response) ? parse(response.body) : nil
   end
-  
+
   # Deletes a device token.
-  # 
+  #
   # @param [String] device_token
   # @return [Boolean] whether or not the deletion was successful
   def delete_device_token(device_token)
     response = @connection.delete(device_token_uri(device_token))
     successful?(response)
   end
-  
+
   # Registers an APID.
-  # 
+  #
   # @param [String] apid
   # @param [Hash] payload the payload to send during registration
   # @return [Boolean] whether or not the registration was successful
   def register_apid(apid, payload = {})
     uri = apid_uri(apid)
-    
+
     if payload.empty?
       response = @connection.put(uri)
     else
       response = @connection.put(uri, payload, JSON_HEADERS)
     end
-    
+
     successful?(response)
   end
-  
+
   # Retrieves information on an APID.
-  # 
+  #
   # @param [String] apid
   # @return [Hash, nil]
   def apid(apid)
     response = @connection.get(apid_uri(apid))
     successful?(response) ? parse(response.body) : nil
   end
-  
+
   # Deletes an APID.
-  # 
+  #
   # @param [String] apid
   # @return [Boolean] whether or not the deletion was successful
   def delete_apid(apid)
     response = @connection.delete(apid_uri(apid))
     successful?(response)
   end
-  
+
   # Pushes a message.
-  # 
+  #
   # @param [Hash] payload the payload of the message
   # @return [Boolean] whether or not pushing the message was successful
   def push(payload)
     response = @connection.post(PUSH_URI, payload, JSON_HEADERS)
     successful?(response)
   end
-  
+
   # Batch pushes multiple messages.
-  # 
+  #
   # @param [<Hash>] payload the payloads of each message
   # @return [Boolean] whether or not pushing the messages was successful
   def batch_push(*payload)
     response = @connection.post(BATCH_PUSH_URI, payload, JSON_HEADERS)
     successful?(response)
   end
-  
+
   # Broadcasts a message.
-  # 
+  #
   # @param [Hash] payload the payload of the message
   # @return [Boolean] whether or not broadcasting the message was successful
   def broadcast(payload)
     response = @connection.post(BROADCAST_URI, payload, JSON_HEADERS)
     successful?(response)
   end
-  
+
   # Retrieves feedback on device tokens.
-  # 
+  #
   # This is useful for removing inactive device tokens for the database.
-  # 
+  #
   # @param [Time] since the time to retrieve inactive tokens from
   # @return [Hash, nil]
   def feedback(since)
@@ -209,17 +209,17 @@ class Zeppelin
     response = @connection.delete(device_tag_uri(device_token, tag_name))
     successful?(response)
   end
-  
+
   private
-  
+
   def device_token_uri(device_token)
     "/api/device_tokens/#{device_token}"
   end
-  
+
   def apid_uri(apid)
     "/api/apids/#{apid}"
   end
-  
+
   def feedback_uri(since)
     "/api/device_tokens/feedback/?since=#{since.utc.iso8601}"
   end
@@ -231,7 +231,7 @@ class Zeppelin
   def device_tag_uri(device_token, tag_name)
     device_token_uri(device_token) + "/tags/#{tag_name}"
   end
-  
+
   def successful?(response)
     SUCCESSFUL_STATUS_CODES.include?(response.status)
   end
