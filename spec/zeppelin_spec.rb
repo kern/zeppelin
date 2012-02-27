e66cb9a3d3eafdb8d0a72eb5da7ced657cc1869a
James Herdman <james.herdman@me.com>
2012-02-27 13:52:00 -0500

    Tidy up testing

--- spec/zeppelin_spec.rb
+++ spec/zeppelin_spec.rb
@@ -38,13 +38,13 @@ describe Zeppelin do
   end
 
   describe '#register_device_token' do
+    let(:uri) { "/api/device_tokens/#{device_token}" }
+
     let(:payload) { { :alias => 'CapnKernul' } }
 
     it 'registers a device with the service' do
       stub_requests do |stub|
-        stub.put("/api/device_tokens/#{device_token}") do |stub|
-          [201, {}, '']
-        end
+        stub.put(uri) { [201, {}, ''] }
       end
 
       subject.register_device_token(device_token).should be_true
@@ -52,19 +52,15 @@ describe Zeppelin do
 
     it 'accepts a payload' do
       stub_requests do |stub|
-        stub.put("/api/device_tokens/#{device_token}", MultiJson.encode(payload)) do
-          [200, {}, '']
-        end
+        stub.put(uri) { [201, {}, ''] }
       end
 
-      subject.register_device_token(device_token, payload).should be_true
+      subject.register_device_token(device_token, payload)
     end
 
     it 'responds with false when an error occurs' do
       stub_requests do |stub|
-        stub.put("/api/device_tokens/#{device_token}", nil) do
-          [500, {}, '']
-        end
+        stub.put(uri) { [500, {}, ''] }
       end
 
       expect {
@@ -74,23 +70,21 @@ describe Zeppelin do
   end
 
   describe '#device_token' do
+    let(:uri) { "/api/device_tokens/#{device_token}" }
+
     let(:response_body) { { 'foo' => 'bar' } }
 
     it 'gets information about a device' do
       stub_requests do |stub|
-        stub.get("/api/device_tokens/#{device_token}") do
-          [200, { 'Content-Type' => 'application/json' }, MultiJson.encode(response_body)]
-        end
+        stub.get(uri) { [200, { 'Content-Type' => 'application/json' }, MultiJson.encode(response_body)] }
       end
 
       subject.device_token(device_token).should eq(response_body)
     end
 
-    it 'is nil when the request fails' do
+    it 'raises an error when the request fails' do
       stub_requests do |stub|
-        stub.get("/api/device_tokens/#{device_token}") do
-          [404, {}, '']
-        end
+        stub.get(uri) { [404, {}, ''] }
       end
 
       expect {
@@ -100,21 +94,19 @@ describe Zeppelin do
   end
 
   describe '#delete_device_token' do
+    let(:uri) { "/api/device_tokens/#{device_token}" }
+
     it 'is true when successful' do
       stub_requests do |stub|
-        stub.delete("/api/device_tokens/#{device_token}") do
-          [204, {}, '']
-        end
+        stub.delete(uri) { [204, {}, ''] }
       end
 
       subject.delete_device_token(device_token).should be_true
     end
 
-    it 'is false when the request fails' do
+    it 'raises an error when the request fails' do
       stub_requests do |stub|
-        stub.delete("/api/device_tokens/#{device_token}") do
-          [404, {}, '']
-        end
+        stub.delete(uri) { [404, {}, ''] }
       end
 
       expect {
@@ -124,13 +116,13 @@ describe Zeppelin do
   end
 
   describe '#register_apid' do
+    let(:uri) { "/api/apids/#{device_token}" }
+
     let(:payload) { { :alias => 'CapnKernul' } }
 
     it 'registers a device with the service' do
       stub_requests do |stub|
-        stub.put("/api/apids/#{device_token}") do |stub|
-          [201, {}, '']
-        end
+        stub.put(uri) { [201, {}, ''] }
       end
 
       subject.register_apid(device_token).should be_true
@@ -138,19 +130,15 @@ describe Zeppelin do
 
     it 'accepts a payload' do
       stub_requests do |stub|
-        stub.put("/api/apids/#{device_token}", MultiJson.encode(payload)) do
-          [200, {}, '']
-        end
+        stub.put(uri, MultiJson.encode(payload)) { [200, {}, ''] }
       end
 
       subject.register_apid(device_token, payload).should be_true
     end
 
-    it 'responds with false when an error occurs' do
+    it 'raises an error when the request fails' do
       stub_requests do |stub|
-        stub.put("/api/apids/#{device_token}", nil) do
-          [500, {}, '']
-        end
+        stub.put(uri) { [500, {}, ''] }
       end
 
       expect {
@@ -160,23 +148,21 @@ describe Zeppelin do
   end
 
   describe '#apid' do
+    let(:uri) { "/api/apids/#{device_token}" }
+
     let(:response_body) { { 'foo' => 'bar' } }
 
     it 'responds with information about a device when request is successful' do
       stub_requests do |stub|
-        stub.get("/api/apids/#{device_token}") do
-          [200, { 'Content-Type' => 'application/json' }, MultiJson.encode(response_body)]
-        end
+        stub.get(uri) { [200, { 'Content-Type' => 'application/json' }, MultiJson.encode(response_body)] }
       end
 
       subject.apid(device_token).should eq(response_body)
     end
 
-    it 'is nil when the request fails' do
+    it 'raises an error when the request fails' do
       stub_requests do |stub|
-        stub.get("/api/apids/#{device_token}") do
-          [404, {}, '']
-        end
+        stub.get("/api/apids/#{device_token}") { [404, {}, ''] }
       end
 
       expect {
@@ -186,21 +172,19 @@ describe Zeppelin do
   end
 
   describe '#delete_apid' do
+    let(:uri) { "/api/apids/#{device_token}" }
+
     it 'responds with true when request successful' do
       stub_requests do |stub|
-          stub.delete("/api/apids/#{device_token}") do
-          [204, {}, '']
-        end
+          stub.delete(uri) { [204, {}, ''] }
       end
 
       subject.delete_apid(device_token).should be_true
     end
 
-    it 'responds with false when request fails' do
+    it 'raises an error when the request fails' do
       stub_requests do |stub|
-        stub.delete("/api/apids/#{device_token}") do
-          [404, {}, '']
-        end
+        stub.delete(uri) { [404, {}, ''] }
       end
 
       expect {
@@ -210,23 +194,21 @@ describe Zeppelin do
   end
 
   describe '#push' do
+    let(:uri) { '/api/push/' }
+
     let(:payload) { { :device_tokens => [device_token], :aps => { :alert => 'Hello from Urban Airship!' } } }
 
     it 'is true when the request is successful' do
       stub_requests do |stub|
-        stub.post('/api/push/', MultiJson.encode(payload)) do
-          [200, {}, '']
-        end
+        stub.post(uri, MultiJson.encode(payload)) { [200, {}, ''] }
       end
 
       subject.push(payload).should be_true
     end
 
-    it 'is false when the request fails' do
+    it 'raises an error when the request fails' do
       stub_requests do |stub|
-        stub.post('/api/push/', '{}') do
-          [400, {}, '']
-        end
+        stub.post(uri, '{}') { [400, {}, ''] }
       end
 
       expect {
@@ -252,21 +234,19 @@ describe Zeppelin do
 
     let(:payload) { [message1, message2] }
 
+    let(:uri) { '/api/push/batch/' }
+
     it 'is true when the request was successful' do
       stub_requests do |stub|
-        stub.post('/api/push/batch/', MultiJson.encode(payload)) do
-         [200, {}, '']
-        end
+        stub.post(uri, MultiJson.encode(payload)) { [200, {}, ''] }
       end
 
       subject.batch_push(message1, message2).should be_true
     end
 
-    it 'is false when the request fails' do
+    it 'raises an error when the request fails' do
       stub_requests do |stub|
-        stub.post('/api/push/batch/', '[{},{}]') do
-          [400, {}, '']
-        end
+        stub.post('/api/push/batch/', '[{},{}]') { [400, {}, ''] }
       end
 
       expect {
@@ -278,21 +258,19 @@ describe Zeppelin do
   describe '#broadcast' do
     let(:payload) {  { :aps => { :alert => 'Hello from Urban Airship!' } } }
 
+    let(:uri) { '/api/push/broadcast/' }
+
     it 'is true when the request is successful' do
       stub_requests do |stub|
-        stub.post('/api/push/broadcast/', MultiJson.encode(payload)) do
-          [200, {}, '']
-        end
+        stub.post(uri, MultiJson.encode(payload)) { [200, {}, ''] }
       end
 
       subject.broadcast(payload).should be_true
     end
 
-    it 'is false when the request fails' do
+    it 'raises an error when the request fails' do
       stub_requests do |stub|
-        stub.post('/api/push/broadcast/', '{}') do
-          [400, {}, '']
-        end
+        stub.post(uri, '{}') { [400, {}, ''] }
       end
 
       expect {
@@ -306,21 +284,19 @@ describe Zeppelin do
 
     let(:since) { Time.at(0) }
 
+    let(:uri) { '/api/device_tokens/feedback/?since=1970-01-01T00%3A00%3A00Z' }
+
     it 'is the response body for a successful request' do
       stub_requests do |stub|
-        stub.get('/api/device_tokens/feedback/?since=1970-01-01T00%3A00%3A00Z') do
-          [200, { 'Content-Type' => 'application/json' }, MultiJson.encode(response_body)]
-        end
+        stub.get(uri) { [200, { 'Content-Type' => 'application/json' }, MultiJson.encode(response_body)] }
       end
 
       subject.feedback(since)
     end
 
-    it 'is nil when the request fails' do
+    it 'raises an error when the request fails' do
       stub_requests do |stub|
-        stub.get('/api/device_tokens/feedback/?since=1970-01-01T00%3A00%3A00Z') do
-          [400, {}, '']
-        end
+        stub.get(uri) { [400, {}, ''] }
       end
 
       expect {
@@ -336,9 +312,7 @@ describe Zeppelin do
 
     it 'requets to modify device tokens on a tag' do
       stub_requests do |stub|
-        stub.post("/api/tags/#{tag_name}") do
-          [200, {}, 'OK']
-        end
+        stub.post("/api/tags/#{tag_name}") { [200, {}, 'OK'] }
       end
 
       subject.modify_device_tokens_on_tag(tag_name, { 'device_tokens' => { 'add' => [device_token] } }).should be
@@ -350,9 +324,7 @@ describe Zeppelin do
 
     it 'is true when the request is successful' do
       stub_requests do |stub|
-        stub.put("/api/tags/#{tag_name}") do
-          [201, {}, '']
-        end
+        stub.put("/api/tags/#{tag_name}") { [201, {}, ''] }
       end
 
       subject.add_tag(tag_name).should be_true
@@ -364,9 +336,7 @@ describe Zeppelin do
 
     it 'is true when the request is successful' do
       stub_requests do |stub|
-        stub.delete("/api/tags/#{tag_name}") do
-          [204, {}, '']
-        end
+        stub.delete("/api/tags/#{tag_name}") { [204, {}, ''] }
       end
 
       subject.remove_tag(tag_name).should be_true
@@ -374,9 +344,7 @@ describe Zeppelin do
 
     it 'is false when the request fails' do
       stub_requests do |stub|
-        stub.delete("/api/tags/#{tag_name}") do
-          [404, {}, '']
-        end
+        stub.delete("/api/tags/#{tag_name}") { [404, {}, ''] }
       end
 
       expect {
@@ -388,21 +356,19 @@ describe Zeppelin do
   describe '#device_tags' do
     let(:response_body) { { 'tags' => ['tag1', 'some_tag'] } }
 
+    let(:uri) { "/api/device_tokens/#{device_token}/tags/" }
+
     it 'is the collection of tags on a device when request is successful' do
       stub_requests do |stub|
-        stub.get("/api/device_tokens/#{device_token}/tags/") do
-          [200, { 'Content-Type' => 'application/json' }, MultiJson.encode(response_body)]
-        end
+        stub.get(uri) { [200, { 'Content-Type' => 'application/json' }, MultiJson.encode(response_body)] }
       end
 
       subject.device_tags(device_token).should eq(response_body)
     end
 
-    it 'is nil when the request fails' do
+    it 'raises an error when the request fails' do
       stub_requests do |stub|
-        stub.get("/api/device_tokens/#{device_token}/tags/") do
-          [404, {}, 'Not Found']
-        end
+        stub.get(uri) { [404, {}, 'Not Found'] }
       end
 
       expect {
@@ -414,21 +380,19 @@ describe Zeppelin do
   describe '#add_tag_to_device' do
     let(:tag_name) { 'radio.head' }
 
+    let(:uri) { "/api/device_tokens/#{device_token}/tags/#{tag_name}" }
+
     it 'is true when the request is successful' do
       stub_requests do |stub|
-        stub.put("/api/device_tokens/#{device_token}/tags/#{tag_name}") do
-          [201, {}, 'Created']
-        end
+        stub.put(uri) { [201, {}, 'Created'] }
       end
 
       subject.add_tag_to_device(device_token, tag_name).should be_true
     end
 
-    it 'is false when the request fails' do
+    it 'raises an error when the request fails' do
       stub_requests do |stub|
-        stub.put("/api/device_tokens/#{device_token}/tags/#{tag_name}") do
-          [404, {}, '']
-        end
+        stub.put(uri) { [404, {}, ''] }
       end
 
       expect {
@@ -440,21 +404,19 @@ describe Zeppelin do
   describe '#remove_tag_from_device' do
     let(:tag_name) { 'martin.fowler' }
 
+    let(:uri) { "/api/device_tokens/#{device_token}/tags/#{tag_name}" }
+
     it 'is true when the request is successful' do
       stub_requests do |stub|
-        stub.delete("/api/device_tokens/#{device_token}/tags/#{tag_name}") do
-          [204, {}, 'No Content']
-        end
+        stub.delete(uri) { [204, {}, 'No Content'] }
       end
 
       subject.remove_tag_from_device(device_token, tag_name).should be_true
     end
 
-    it 'is false when the request fails' do
+    it 'raises an error when the request fails' do
       stub_requests do |stub|
-        stub.delete("/api/device_tokens/#{device_token}/tags/#{tag_name}") do
-          [404, {}, '']
-        end
+        stub.delete(uri) { [404, {}, ''] }
       end
 
       expect {
@@ -463,8 +425,10 @@ describe Zeppelin do
     end
   end
 
-  def stub_requests(&block)
+  def stub_requests
     subject.connection.builder.handlers.delete(Faraday::Adapter::NetHttp)
-    subject.connection.adapter(:test, &block)
+    subject.connection.adapter :test do |stubs|
+      yield(stubs)
+    end
   end
 end
