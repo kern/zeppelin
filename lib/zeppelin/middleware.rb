9905e5f061e7d33843957d1607fbd77ab924570c
Alex Chee <alexchee11@gmail.com>
2014-04-29 15:24:10 -0700

    updated to be compatible with Faraday 0.9.0, I think
    updated to be compatible with Faraday 0.9.0, I think

--- lib/zeppelin/middleware.rb
+++ lib/zeppelin/middleware.rb
@@ -5,4 +5,9 @@ end
 
 require 'zeppelin/middleware/response_raise_error'
 
-Faraday.register_middleware :response, zeppelin_raise_error: Zeppelin::Middleware::ResponseRaiseError
+# For backwards compatibility with Faraday < 0.9
+if Faraday.respond_to?(:register_middleware)
+  Faraday.register_middleware :response, zeppelin_raise_error: Zeppelin::Middleware::ResponseRaiseError
+else
+  Faraday::Response.register_middleware zeppelin_raise_error: Zeppelin::Middleware::ResponseRaiseError
+end
